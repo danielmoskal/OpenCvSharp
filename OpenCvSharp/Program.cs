c03b4b20df89dec4f5c2213eea1a5d6c02582a06
Daniel Moskal <daniel19967@gmail.com>
2018-11-22 23:48:39 +0100

    init

---
+++ OpenCvSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenCvSharp.Infrastructure.Abstract;
+using OpenCvSharp.Infrastructure.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCvSharp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ISample sample = new SiftSurf();
+
+            sample.Run();
+        }
+    }
+}
